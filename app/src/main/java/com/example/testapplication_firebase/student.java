7260f4b1e80b7a66c1aac4c28d9e0e5f03d0b94c
sachini1996 <sachini.nk1996@gmail.com>
2020-05-04 19:29:24 +0530

    firebase

---
+++ app/src/main/java/com/example/testapplication_firebase/student.java
@@ -0,0 +1,45 @@
+package com.example.testapplication_firebase;
+
+public class student {
+    private String ID;
+    private String name;
+    private String address;
+    private int conNo;
+
+    public student() {
+
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getConNo() {
+        return conNo;
+    }
+
+    public void setConNo(int conNo) {
+        this.conNo = conNo;
+    }
+
+}
